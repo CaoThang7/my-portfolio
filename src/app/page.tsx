3c15bec1ef2421acbdd28fba7d91851ce00f007a
Cao Thắng Lý <thangly2k1@gmail.com>
2025-04-17 19:26:43 +0700

    create Progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Progress from "@/components/ScrollProgress/Progress";
 import DarkModeSwitcher from "@/components/Theme/DarkModeSwitcher";
 
 const Home = () => {
   return (
     <>
       <DarkModeSwitcher />
+      <Progress />
       <div className="w-full max-w-[1024px] min-h-screen bg-[var(--bgColor)] absolute top-0 left-1/2 -translate-x-1/2 px-[15px] overflow-hidden">
         {/* Content here */}
       </div>
---
+++ src/components/ScrollProgress/Progress.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+const Progress = () => {
+  const [scroll, setScroll] = useState(0);
+  const progressRef = useRef<HTMLDivElement>(null);
+  const valueRef = useRef<HTMLDivElement>(null);
+
+  const updateScrollProgress = () => {
+    const pos = document.documentElement.scrollTop;
+    const calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrollValue = Math.round((pos * 100) / calcHeight);
+    setScroll(scrollValue);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", updateScrollProgress);
+    window.addEventListener("load", updateScrollProgress);
+    return () => {
+      window.removeEventListener("scroll", updateScrollProgress);
+      window.removeEventListener("load", updateScrollProgress);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={progressRef}
+      className="w-[55px] h-[55px] rounded-full fixed bottom-6 right-6 z-[999] cursor-pointer shadow-[0_0_10px_var(--whiteColor)] flex items-center justify-center"
+      style={{
+        background: `conic-gradient(#38bdf8 ${scroll}%, #0f172a ${scroll}%)`,
+      }}
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
+      <div
+        ref={valueRef}
+        className="w-[calc(100%-5px)] h-[calc(100%-5px)] bg-[#111] rounded-full flex items-center justify-center text-white"
+      >
+        {scroll}%
+      </div>
+    </div>
+  );
+};
+
+export default Progress;
